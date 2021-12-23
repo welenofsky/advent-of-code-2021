617cc0b8f1a25c1739acccbdc7a4b77d17b0627d
Justin Welenofsky <welenofsky@gmail.com>
2021-12-22 22:03:28 -0800

    Day 6 - Refactor

--- day_6/solution.py
+++ day_6/solution.py
@@ -1,23 +1,15 @@
 def part_1_solution(inputdata):
-    fish_counts = parse_input_data(inputdata)
-
-    for i in range(1, 81):
-        new_counts = fish_counts.copy()
-        for j in range(8, -1, -1):
-            if j == 8:
-                new_counts['8'] = fish_counts['0']
-            elif j == 6:
-                new_counts['6'] = fish_counts['0'] + fish_counts['7']
-            else:
-                new_counts[f"{j}"] = fish_counts[f"{j+1}"]
-        fish_counts = new_counts
-    return sum([i for i in fish_counts.values()])
+    return solution(inputdata, 80)
 
 
 def part_2_solution(inputdata):
+    return solution(inputdata, 256)
+
+
+def solution(inputdata, days):
     fish_counts = parse_input_data(inputdata)
 
-    for i in range(1, 257):
+    for i in range(1, days + 1):
         new_counts = fish_counts.copy()
         for j in range(8, -1, -1):
             if j == 8:
